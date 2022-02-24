83917badf4cd694947a4c31d7b355b9e5461730b
Cornelius Behrend <cornelius@staffbase.com>
2022-02-24 09:47:11 +0100

    chore: initial ios project

---
+++ ios/RichPush/RichPush/RichPushApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichPushApp.swift
+//  RichPush
+//
+//  Created by Cornelius Behrend on 24.02.22.
+//
+
+import SwiftUI
+
+@main
+struct RichPushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
